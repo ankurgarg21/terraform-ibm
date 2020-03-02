4a205276cf369110c517d756c49434faccb4314e
Ankur Garg <mail.syswellinfotech@gmail.com>
2020-03-02 09:47:34 +0800

    create vm in classic infra

---
+++ create-vm-classic-infra.tf
@@ -0,0 +1,13 @@
+resource "ibm_compute_vm_instance" "virtualserver" {
+ hostname = "virtualserver"
+ domain = "Non-prod-cloud"
+ os_reference_code = "DEBIAN_8_64"
+ datacenter = "sng01"
+ network_speed = 10
+ hourly_billing = true
+ private_network_only = false
+ cores = 1
+ memory = 1024
+ disks = [25]
+ local_disk = false
+}
