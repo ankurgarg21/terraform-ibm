cef1ebf16fada33cc3ae66f1b012f6da970a30de
Ankur Garg <ankurgarg21@users.noreply.github.com>
2020-03-02 10:29:07 +0800

    updated template

--- create-vm-classic-infra.tf
+++ create-vm-classic-infra.tf
@@ -1,6 +1,6 @@
 resource "ibm_compute_vm_instance" "virtualserver" {
  hostname = "virtualserver02"
- domain = "Non-prod-cloud.com"
+ domain = "Nonprodcloud"
  os_reference_code = "DEBIAN_8_64"
  datacenter = "sng01"
  network_speed = 10
